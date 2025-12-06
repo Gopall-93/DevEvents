47d36ec933900f8178cc7baed5dfb20c83b470d8
gopall-93 <gopallsharma93@gmail.com>
2025-12-07 03:27:49 +0530

    Implement event creation and fetching API routes with Cloudinary integration

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -4,15 +4,15 @@ import Link from 'next/link'
 interface Props{
     title:string,
     image:string,
-    sluge:string,
+    slug:string,
     location:string,
     date:string,
     time:string
 }
 
-const EventCard = ({title,image,sluge,location,date,time}:Props) => {
+const EventCard = ({title,image,slug,location,date,time}:Props) => {
   return (
-    <Link href={`/events/${sluge}`} id='event-card'>
+    <Link href={`/events/${slug}`} id='event-card'>
         <Image src={image} alt={title} width={410} height={300} className='poster'/>
         <div className='flex fles-row gap-2'>
             <Image src="/icons/pin.svg" alt='location' width={14} height={14}/>
