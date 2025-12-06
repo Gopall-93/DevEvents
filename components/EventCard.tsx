675746d21148a281410315ead072120bbe500e1d
gopall-93 <gopallsharma93@gmail.com>
2025-12-06 20:00:57 +0530

    Added the event card

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 interface Props{
     title:string,
     image:string,
-    slug:string,
+    sluge:string,
     location:string,
     date:string,
     time:string
 }
 
-const EventCard = ({title,image,slug,location,date,time}:Props) => {
+const EventCard = ({title,image,sluge,location,date,time}:Props) => {
   return (
     <Link href={`/events`} id='event-card'>
         <Image src={image} alt={title} width={410} height={300} className='poster'/>
