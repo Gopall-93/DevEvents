d749eb29f7458cf3d32786ac14a616a0692d94d4
gopall-93 <gopallsharma93@gmail.com>
2025-12-06 21:11:01 +0530

    Integrate PostHog for event tracking and enhance EventCard with additional details

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
       <ExploreBtn/>
       <div className='mt-20 space-y-7'>
         <h3>Featured Events</h3>
-        <ul className='events'>
+        <ul className='events list-none'>
           {
             EVENTS.map((event)=>(
               <li key={event.title}>
--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+
 interface Props{
     title:string,
     image:string,
@@ -12,8 +12,25 @@ interface Props{
 
 const EventCard = ({title,image,sluge,location,date,time}:Props) => {
   return (
-    <Link href={`/events`} id='event-card'>
+    <Link href={`/events/${sluge}`} id='event-card'>
         <Image src={image} alt={title} width={410} height={300} className='poster'/>
+        <div className='flex fles-row gap-2'>
+            <Image src="/icons/pin.svg" alt='location' width={14} height={14}/>
+            <p>{location}</p>
+        </div>
+        <p className='title'>{title}</p>
+
+        <div className='datetime'>
+            <div>
+                <Image src="/icons/calendar.svg" alt='date' width={14} height={14}/>
+            <p>{date}</p>
+            </div>
+            <div>
+                <Image src="/icons/clock.svg" alt='time' width={14} height={14}/>
+            <p>{time}</p>
+            </div>
+        </div>
+        
     </Link>
   )
 }
