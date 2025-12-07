75197bfccceadd42abe8d7f17c9d6430e253df39
gopall-93 <gopallsharma93@gmail.com>
2025-12-07 21:53:55 +0530

    Refactor event fetching logic to simplify API call and improve caching; remove local development fallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,16 @@
 import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
 import { IEvent } from '@/database/event.model'
+import { cacheLife } from 'next/cache'
+import { events } from '@/lib/constants'
 
-// Force SSR so fetch doesn't run at build time
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
-const page = async () => {
-
-  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-  // Fallback for local dev
-  const safeUrl = BASE_URL 
-    ? `${BASE_URL}/api/events`
-    : `http://localhost:3000/api/events`;
-
-  let events: IEvent[] = [];
-
-  try {
-    const response = await fetch(safeUrl, { cache: "no-store" });
-    const data = await response.json();
-    events = data.events ?? [];
-  } catch (error) {
-    console.error("[Home Fetch Error]:", error);
-  }
-
+const page = async() => {
+  'use cache'
+  cacheLife('hours')
+  const response = await fetch(`${BASE_URL}/api/events`)
+  const {events} = await response.json()
   return (
     <section>
       <h1 className='text-center'>The Hub For Every Dev <br /> Event You Can't Miss</h1>
@@ -33,19 +19,17 @@ const page = async () => {
       <div className='mt-20 space-y-7'>
         <h3>Featured Events</h3>
         <ul className='events list-none'>
-          {events.length > 0 ? (
-            events.map((event: IEvent) => (
+          {
+            events&&events.length>0&&events.map((event:IEvent)=>(
               <li key={event.title}>
-                <EventCard {...event} />
+                <EventCard {...event}/>
               </li>
             ))
-          ) : (
-            <p className="text-light-200">No events found.</p>
-          )}
+          }
         </ul>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default page;
+export default page
